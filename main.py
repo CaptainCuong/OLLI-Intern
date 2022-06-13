7639480152cb75f897b8f53caaac47b80e94e233
CaptainCuong <captaincuong1692001@gmail.com>
2022-06-13 08:50:49 +0700

    add lstm

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 from utils import *
+from collections import Counter
+import re
+import nltk
+import numpy as np
 
-# s = 'Tôi có hai ba bốn cái kẹo'
-# print(clean_num(s))
+train_file = open('lstm/train.ft.txt','rb')
+test_file = open('lstm/test.ft.txt','rb')
 
-# file = open('testcase.csv', 'r', encoding='UTF-8')
-# sntns = file.readlines()
-# for sntn in sntns:
-# 	print(sntn)
-# 	print(convert_to_litnum(sntn))
-# 	print('---------------------------------')
-# file.close()
+train_file = train_file.readlines()
+test_file = test_file.readlines()
 
-import pandas as pd
-file1 = pd.read_csv('48_nghia_mic.csv')
-file2 = pd.read_csv('2_hongnhu_mic.csv')
-stns = pd.concat([file1['Groundtruth'],file1['Groundtruth']],axis=0)
-for stn in stns:
-	print(stn)
-	print(token_label(stn.split()))
-	print('---------------------------------')
-# pd.concat([stns,stns.map(lambda x:' '.join(token_label(x.split())))],axis=1).to_csv('dataset.csv',index=False)
+######### Extract training/testing data #########
+
+num_train = 80000  # We're training on the first 800,000 reviews in the dataset
+num_test = 20000  # Using 200,000 reviews from test set
+
+train_file = [x.decode('utf-8') for x in train_file[:num_train]]
+test_file = [x.decode('utf-8') for x in test_file[:num_test]]
+
+# Extracting labels from sentences
+train_labels = [0 if x.split(' ')[0] == '__label__1' else 1 for x in train_file]
+train_sentences = [x.split(' ', 1)[1][:-1].lower() for x in train_file]
+
+test_labels = [0 if x.split(' ')[0] == '__label__1' else 1 for x in test_file]
+test_sentences = [x.split(' ', 1)[1][:-1].lower() for x in test_file]
+
+# Some simple cleaning of data
+for i in range(len(train_sentences)):
+    train_sentences[i] = re.sub(r'\d','0',train_sentences[i])
+
+for i in range(len(test_sentences)):
+    test_sentences[i] = re.sub(r'\d','0',test_sentences[i])
+
+# Modify URLs to <url>
+for i in range(len(train_sentences)):
+    if 'www.' in train_sentences[i] or 'http:' in train_sentences[i] or 'https:' in train_sentences[i] or '.com' in train_sentences[i]:
+        # [^ ]+(?<=\.[a-z]{3}): All strings without spaces end with .xxx
+        train_sentences[i] = re.sub(r"([^ ]+(?<=\.[a-z]{3}))", "<url>", train_sentences[i])
+        
+for i in range(len(test_sentences)):
+    if 'www.' in test_sentences[i] or 'http:' in test_sentences[i] or 'https:' in test_sentences[i] or '.com' in test_sentences[i]:
+        test_sentences[i] = re.sub(r"([^ ]+(?<=\.[a-z]{3}))", "<url>", test_sentences[i])
