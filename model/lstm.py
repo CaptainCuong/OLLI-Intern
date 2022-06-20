667116e8989974a59f59f50f5d41183b2b99dd6b
CaptainCuong <captaincuong1692001@gmail.com>
2022-06-20 12:47:24 +0700

    update 20/6

--- model/lstm.py
+++ model/lstm.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 
-
 # torch.cuda.is_available() checks and returns a Boolean True if a GPU is available, else it'll return False
 is_cuda = torch.cuda.is_available()
 
@@ -12,35 +11,79 @@ else:
     device = torch.device("cpu")
 
 class NER_LSTMNet(nn.Module):
-    def __init__(self, vocab_size, output_size, embedding_dim, hidden_dim, n_layers, drop_prob=0.5):
+    def __init__(self, vocab_size, n_entity, output_size, embedding_dim, hidden_dim, n_layers, seq_len = 20, drop_prob=0.5, batch_size = 1):
         super(NER_LSTMNet, self).__init__()
         self.output_size = output_size
         self.n_layers = n_layers
         self.hidden_dim = hidden_dim
+        self.seq_len = seq_len
+        self.batch_size = batch_size
         
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, dropout=drop_prob, batch_first=True)
+        self.lstm_encode = nn.LSTM(embedding_dim, hidden_dim, n_layers, dropout=drop_prob, batch_first=True)
+        self.lstm_decode = nn.LSTM(output_size, hidden_dim, n_layers, dropout=drop_prob, batch_first=True)
         self.dropout = nn.Dropout(drop_prob)
-        self.fc = nn.Linear(hidden_dim, output_size)
+        self.fclst = [nn.Linear(hidden_dim, output_size) for i in range(seq_len)]
         self.sigmoid = nn.Sigmoid()
-        
-    def forward(self, x, hidden):
-        batch_size = x.size(0)
+        self.softmaxlst = [nn.Softmax(2) for i in range(seq_len)]
+ 
+    def forward(self, x):
+        '''
+        x: sequence of encoded words
+        shape of x: [batchsize, seq_len, embedding_dim]
+
+        hidden: initial hidden
+        '''
+        self.batch_size = x.size(0)
         x = x.long()
-        embeds = self.embedding(x)
-        lstm_out, hidden = self.lstm(embeds, hidden)
-        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
-        
-        out = self.dropout(lstm_out)
-        out = self.fc(out)
-        out = self.sigmoid(out)
-        
-        out = out.view(batch_size, -1)
-        out = out[:,-1]
-        return out, hidden
+        hidden = self.init_hidden(self.batch_size)
+        encode_in = self.embedding(x)
+        encode_out, hidden = self.lstm_encode(encode_in, hidden)
+ 
+        pred_entity = []
+        in_decode = torch.zeros([self.batch_size, 1, self.output_size])
+        for i in range(self.batch_size):
+            in_decode[i][0][0] = 1
+        out_decode = None
+        for i in range(self.seq_len):
+            out_decode, hidden = self.lstm_decode(in_decode, hidden)
+            out_decode = self.fclst[i](out_decode)
+            out_decode = self.softmaxlst[i](out_decode)
+            pred_entity.append(out_decode)
+
+            in_decode = torch.zeros([self.batch_size, 1, self.output_size])
+            max_ind = torch.argmax(out_decode, dim = 2)
+            for i in range(self.batch_size):
+                in_decode[i][0][max_ind[i][0]] = 1
+ 
+        return torch.cat(pred_entity, dim = 1)
     
     def init_hidden(self, batch_size):
         weight = next(self.parameters()).data
         hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().to(device),
                       weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().to(device))
         return hidden
+ 
+    def train(train_loader, epochs, batch_size, learning_rate, criterion, optimizer, clip):
+        '''
+        train_loader:
+            input: (batch_size, seq_len, embedding_dim)
+            output: (batch_size, seq_len, out_dim)
+        '''
+        self.batch_size = batch_size
+        assert train_loader[0].size(1) == self.seq_len
+        assert criterion in ['BCE']
+        assert optimizer in ['Adam']
+        if criterion == 'BCE':
+            criterion = nn.BCELoss()
+        if optimizer == 'Adam':
+            optimizer = torch.optim.Adam(self.parameters(), lr = learning_rate)
+        self.train()
+ 
+        for i in range(epochs):
+            for inputs, labels in train_loader:
+                out_decode = self(inputs)
+                self.zero_grad()
+                loss = criterion(out_decode, labels)
+                loss.backward()
+                optimizer.step()
--- utils/helper.py
+++ utils/helper.py
@@ -1,78 +1,79 @@
-from .token import num, post_num, flt, num_mag, sub_num_mag, unit
+from .token import flt, num, num_mag, post_num, sub_num_mag, unit
+
 
 def convert_to_litnum(str_in):
     words = str_in.split()
     swap_flt(words)
-	# print(words)
+    # print(words)
     words = lit2num(words)
     return ' '.join(words)
 
 def token_label(ws):
     words = ws.copy()
     for i,w in enumerate(words):
-    	if w in num:
-    		words[i] = 'num'
-    	elif w in post_num:
-    		words[i] = 'post_num'
-    	elif w in flt:
-    		words[i] = 'flt'
-    	elif w in unit:
-    		words[i] = 'unit'
-    	elif w in num_mag:
-    		words[i] = 'num_mag'
-    	elif w in sub_num_mag:
-    		words[i] = 'sub_num_mag'
-    	else:
-    		words[i] = 'unknown'
+        if w in num:
+            words[i] = 'num'
+        elif w in post_num:
+            words[i] = 'post_num'
+        elif w in flt:
+            words[i] = 'flt'
+        elif w in unit:
+            words[i] = 'unit'
+        elif w in num_mag:
+            words[i] = 'num_mag'
+        elif w in sub_num_mag:
+            words[i] = 'sub_num_mag'
+        else:
+            words[i] = 'unknown'
     return words
 
 def swap_flt(words):
     for i in swap_flt_ind(token_label(words)):
-    	temp = words[i]
-    	words[i] = words[i-1]
-    	words[i-1] = temp
+        temp = words[i]
+        words[i] = words[i-1]
+        words[i-1] = temp
 
 def swap_flt_ind(words):
     for i in range(1, len(words)):
-    	if words[i] == 'flt':
-    		yield i
+        if words[i] == 'flt':
+            yield i
 
 def clean_num(words):
     i = 0
     rt = []
     words = words.split()
     while i < len(words):
-    	if words[i] in num | post_num | flt | num_mag or words[i] in sub_num_mag:
-    		st = i
-    		while i < len(words) and (words[i] in num | post_num | flt | num_mag or words[i] in sub_num_mag):
-    			i += 1
-    		lt = i
-    		rt.append(lit2num(words[st:lt]))
-    		i -= 1
-    	else:
-    		rt.append(words[i])
-    	i += 1
+        if words[i] in num | post_num | flt | num_mag or words[i] in sub_num_mag:
+            st = i
+            while i < len(words) and (words[i] in num | post_num | flt | num_mag or words[i] in sub_num_mag):
+                i += 1
+            lt = i
+            rt.append(lit2num(words[st:lt]))
+            i -= 1
+        else:
+            rt.append(words[i])
+        i += 1
     return ' '.join(rt)
 
 def lit2num(words):
     i = 0
     stk = [0]
-	# words = words.split()
+    # words = words.split()
     swap_flt(words)
     while i < len(words):
-    	if words[i] in num:
-    		num_con = []
-    		while i < len(words) and words[i] in num:
-    			num_con.append(num[words[i]])
-    			i += 1
-    		i -= 1
-    		stk.append(float(''.join(num_con)))
+        if words[i] in num:
+            num_con = []
+            while i < len(words) and words[i] in num:
+                num_con.append(num[words[i]])
+                i += 1
+            i -= 1
+            stk.append(float(''.join(num_con)))
 
-    	elif words[i] in num_mag:
-    		stk[-1] = stk[-1]*float(num_mag[words[i]])
+        elif words[i] in num_mag:
+            stk[-1] = stk[-1]*float(num_mag[words[i]])
 
-    	elif words[i] in flt:
-    		stk[-1] += float(flt[words[i]])
+        elif words[i] in flt:
+            stk[-1] += float(flt[words[i]])
 
-    	i += 1
+        i += 1
     return str(sum(stk))
